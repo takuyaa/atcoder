97708252acbf9315c569104c956871123f0f1766
Takuya Asano <takuya.a@gmail.com>
2022-05-02 22:11:30 +0900

    Remove comment lines

--- typical90/typical90/b.py
+++ typical90/typical90/b.py
@@ -26,38 +26,5 @@ def is_valid(a: Tuple[int, ...]) -> bool:
     return sum == 0
 
 
-# def solve_b(N: int) -> Generator[str, None, None]:
-#     listed: Set[str] = set()
-#     for s in gen(N=N, n=N):
-#         if s in listed:
-#             continue
-#         listed.add(s)
-#         yield s
-
-
-# def gen(N: int, n: int) -> Generator[str, None, None]:
-#     if n <= 0:
-#         yield from ()
-#         return
-#     elif n % 2 != 0:
-#         yield from ()
-#         return
-#     elif n == 2:
-#         yield "()"
-#         return
-
-#     # ( S )
-#     for s in gen(N, n - 2):
-#         yield "(" + s + ")"
-
-#     # S + T
-#     for i in range(1, n // 2):
-#         s_len = (n // 2) - i
-#         t_len = i
-#         for s in gen(N, s_len * 2):
-#             for t in gen(N, t_len * 2):
-#                 yield s + t
-
-
 if __name__ == "__main__":
     main()
