e47546d018c4b99000e9c8be517db5e18b5d842a
Takuya Asano <takuya.a@gmail.com>
2022-05-08 22:15:47 +0900

    Add RMQ for task F

--- typical90/tests/test_f.py
+++ typical90/tests/test_f.py
@@ -1,4 +1,4 @@
-from typical90.f import solve_f
+from typical90.f import RMQ, solve_f
 
 
 def test_solve_f():
@@ -11,3 +11,19 @@ def test_solve_f():
     assert solve_f(5, 3, "edcba") == "cba"
 
     assert solve_f(7, 3, "abcdabc") == "aab"
+
+
+def test_rmq_build():
+    assert RMQ([3]).data[:1] == [3]
+    assert RMQ([3, 1]).data[:3] == [1, 3, 1]
+    assert RMQ([3, 1, 2]).data[:6] == [1, 1, 2, 3, 1, 2]
+    assert RMQ([1, 3, 1, 2]).data[:7] == [1, 1, 1, 1, 3, 1, 2]
+    assert RMQ([1, 3, 1, 2, 3], inf=100).data[:12] == [1, 1, 3, 1, 1, 3, 100, 1, 3, 1, 2, 3]
+
+
+def test_rmq_query():
+    rmq = RMQ([1, 3, 1, 0, 3], inf=100)
+    assert rmq.query(q_left=0, q_right=5) == 0
+    assert rmq.query(q_left=0, q_right=3) == 1
+    assert rmq.query(q_left=4, q_right=5) == 3
+    assert rmq.query(q_left=5, q_right=5) == 100
--- typical90/typical90/f.py
+++ typical90/typical90/f.py
@@ -1,4 +1,6 @@
+import math
 import sys
+from typing import List
 
 sys.setrecursionlimit(1 << 24)
 
@@ -31,6 +33,49 @@ def solve_f(N: int, K: int, S: str) -> str:
             str_min = sub_ans
 
     return str_min
+
+
+class RMQ:
+    def __init__(self, array: List[int], inf: int = sys.maxsize) -> None:
+        self.inf: int = inf
+        self.height: int = 0
+        self.n_leaves: int = 0
+        self.data: List[int]
+
+        if len(array) == 0:
+            self.data = []
+            return
+
+        # hight of segment tree excluding leaves
+        self.height = math.ceil(math.log2(len(array)))
+
+        # number of leaf nodes of segment tree
+        self.n_leaves = 2**self.height
+
+        # internal data representation for segment tree
+        # number of all elements of the segment tree is `2 * N - 1`
+        self.data = [inf] * (2**self.n_leaves - 1)
+
+        # init leaves
+        for i in range(len(array)):
+            self.data[(2**self.height - 1) + i] = array[i]
+
+        for i in range(self.n_leaves - 2, -1, -1):
+            self.data[i] = min(self.data[2 * i + 1], self.data[2 * i + 2])
+
+    def query(self, q_left: int, q_right: int) -> int:
+        return self._query(q_left, q_right, node=0, left=0, right=self.n_leaves)
+
+    def _query(self, q_left: int, q_right: int, node: int, left: int, right: int) -> int:
+        if q_left >= right or q_right <= left:
+            return self.inf
+        elif q_left <= left and q_right >= right:
+            return self.data[node]
+
+        min_l = self._query(q_left, q_right, node=(node * 2 + 1), left=left, right=((left + right) // 2))
+        min_r = self._query(q_left, q_right, node=(node * 2 + 2), left=((left + right) // 2), right=right)
+
+        return min(min_l, min_r)
 
 
 if __name__ == "__main__":
