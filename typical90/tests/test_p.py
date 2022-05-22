28c364dc1051c5f22883b959009a9050a20900a0
Takuya Asano <takuya.a@gmail.com>
2022-05-22 15:54:31 +0900

    Improve performance of task P

--- typical90/tests/test_p.py
+++ typical90/tests/test_p.py
@@ -6,3 +6,6 @@ def test_solve_p():
     assert solve_p(N=9999, A=1, B=5, C=10) == 1004
     assert solve_p(N=998244353, A=314159, B=265358, C=97932) == 3333
     assert solve_p(N=100000000, A=10001, B=10002, C=10003) == 9998
+
+    assert solve_p(N=100, A=100, B=1000, C=1000) == 1
+    assert solve_p(N=100, A=10, B=1, C=1) == 10
